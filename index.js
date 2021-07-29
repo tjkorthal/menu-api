d01669db30d677a737338b228fe9c5c2f7a5417e
Tyler Korthal <tjkorthal@live.com>
2021-07-29 14:28:43 -0400

    Add initial Vendor POST

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function(req, res) {
-  res.send('Hello world')
+// add a vendor
+app.post('/vendor', function(req, res) {
+  res.status(200).end()
 })
 
 app.listen(3000)
